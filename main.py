3a9dfa598eac10f7ee0bb0f1edaf91c04761cf7f
Kevin Pham <kvnvpham@gmail.com>
2022-10-19 19:17:54 -0500

    Change Procfile and Key

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import bleach
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager(app)
